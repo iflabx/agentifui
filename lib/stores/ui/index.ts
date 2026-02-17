29b19126fc184052b6bae0066727a68f59149bb0
bitifirefly <bitifirefly@gmail.com>
2026-02-17 11:07:35 +0000

    chore: prune unused legacy ui and dify modules

--- lib/stores/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './typewriter-store';
-export * from './dropdown-store';
-export * from './file-preview-store';
-export * from './notification-store';
-export * from './tooltip-store';
